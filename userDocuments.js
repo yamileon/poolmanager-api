9e27dc502f3d9535263c86595ce322ffd2406ba8
SulaymanSaleem <39617382+SulaymanSaleem@users.noreply.github.com>
2019-07-02 11:49:46 +0100

    Create userDocuments.js
    users for documents, some of them have empty columns for testing

---
+++ userDocuments.js
@@ -0,0 +1,85 @@
+const usr1 = new users({
+    fname: 'Sulayman',
+    lname: 'Saleem',
+    gamesPlayed:10,
+    wins: 5,
+    losses: 5,
+    winstreak: 4,
+    
+})
+
+const usr2 = new users({
+    fname: 'Danny',
+    lname: 'Lambord',
+    gamesPlayed:20,
+    wins: 16,
+    losses: 4,
+    winstreak: 5,
+    
+})
+
+const usr3 = new users({
+    fname: 'Danny',
+    lname: 'Little',
+    gamesPlayed:17,
+    wins: 14,
+    losses: 3,
+    winstreak: 5,
+    
+})
+const usr4 = new users({
+    fname: 'Ringo',
+    lname: 'Star',
+    gamesPlayed: 3,
+    wins: 0,
+    losses: 3,
+    winstreak: 0,
+    
+})
+const usr5 = new users({
+    fname: 'Dimitrios',
+    lname: 'Papadeas',
+    gamesPlayed: 6,
+    wins: 5,
+    losses: 1,
+    winstreak: 3,
+    
+})
+
+const usr6 = new users({
+    fname: 'Thomas',
+    lname: 'Gilbert',
+    gamesPlayed: 6,
+    wins: 5,
+    losses: 1,
+    winstreak: 3,
+    
+})
+
+const usr7 = new users({
+    fname: 'sdfsf',
+    lname: 'dfhr',
+    gamesPlayed: 6,
+    wins: 5,
+    losses: 1,
+    winstreak: 3,
+    
+})
+
+const usr8 = new users({
+    fname: 'Taefa',
+    lname: 'agreag',
+   
+})
+
+const usr9 = new users({
+    fname: 'sjuyln',
+    lname: 'Kqwet',
+
+    
+})
+
+const usr10 = new users({
+    fname: 'mnbbvv',
+    lname: 'lkjhfd',   
+})
