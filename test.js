157804b233b44fa0402885ebb44b102809ca2f09
dannylambord <dannylambord@hotmail.co.uk>
2019-07-02 16:17:57 +0100

    Working get method for queues

--- test.js
+++ test.js
@@ -1,15 +1,34 @@
-var sch = require('./schmeas/sch')
+var sch = require('./schemas/sch')
 let mong = require('mongoose');
+let express=require('express');
 
-mong.connect('mongodb://localhost/qac', { useNewUrlParser: true }, (err) => {
+mong.connect('mongodb://localhost:27017/27017', { useNewUrlParser: true }, (err) => {
     if (err) { console.log(err); }
     else
         console.log('connected');
 });
 
 
-let test = new sch.rulesets({
-    link:'redx'
+var myModel = mong.model(
+    'queue',
+    this.queueSchema
+);
+
+
+var app = express();
+app.use(express.json());
+
+app.get('/get', async (req, res) => {
+    const docs = await myModel.find();
+    res.send(docs);
 })
 
-test.save()
+app.use((err, _req, res, _next) => {
+    console.error(err);
+return res.status(500).send(err);
+});
+
+
+app.listen(8080, () => {
+console.log('Listening on port 8080');
+});
