4592ec832f8e574e8679325f2eb150ec061ba7e7
TGilbert <thomasgilbert.uk@gmail.com>
2019-07-04 15:59:36 +0100

    put function updating a user's name with a specified one

--- router/user-routes.js
+++ router/user-routes.js
@@ -24,7 +24,8 @@ router.post('/create', (req, res, next) => {
 router.get('/byFirstName', (req, res) =>
     paramHandler(req, res, ['firstName'], () => {
         const { firstName } = req.query;
-        return sch.userModel.find({ firstName }).then(
+        console.log(firstName);
+        return sch.userModel.find({ fname: firstName }).then(
             doc => res.send(doc),
             error => res.send(500).send(error)
         );
@@ -48,27 +49,32 @@ router.delete('/deleteUser', (req, res, next) => {
     })
 });
 
-router.delete('/deleteUserByFirstName/:fname', async (req, res, next) => {
+router.put('/updateUser/:fname/:newUsername',async (req,res,next) => {
     try{
-        console.log(req.params);
-        const {fname} = req.params;
-        await sch.userModel.findOneAndDelete(fname);
-        res.status(204).send();
-    }catch (exception){
-        return next({message: exception.message});
-    }
-});
+        console.log("1",req.params);
+        const {fname, newUsername} = req.params;
+        //const user = sch.userModel.find({fname}).select();
+        //console.log("2",user);
+        //const id = utils.toObjectId(fname);
+        //const id = {_id} = user;
+        //id = utils.toObjectId(id);
+        //const id = user;
+        updateName = `{ fname : ${newUsername} }`;
+        //console.log("3",updateName);
 
-router.put('/updateUser/:fname/:newfname',async (req,res,next) => {
-    try{
-        console.log(req.params);
-        const {fname, newfname} = req.params;
-        return sch.userModel.findOneAndUpdate(fname,newfname,(doc) => {
-            return res.send(doc),console.log("returned ", doc)
+        return sch.userModel.findOneAndUpdate({ fname: fname }, { $set : { fname: newUsername}}).then((user) => {
+            console.log(user);
+            // user.fname = newUsername;
+            // console.log("fname", user.fname);
+            // ;
+            return sch.userModel.findOne({fname: newUsername}).then((updatedUser) => {
+                return res.send(updatedUser),console.log("returned ", updatedUser);
+            })
+            
         });
     } catch (exception) {
         return next({message: exception.message});
     }
-})
+});
 
 module.exports = router;
