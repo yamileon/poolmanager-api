55ccc7ae087dcbdf554135ad323b9db06f8268e6
SulaymanSaleem <39617382+SulaymanSaleem@users.noreply.github.com>
2019-07-05 14:54:13 +0100

    adding danny/sully stuff
    adding our gets and puts and blah blah blah at end of file

--- index.js
+++ index.js
@@ -34,3 +34,55 @@ mongoose.connect(
             console.log(`Server running on port ${config.app.PORT}`);
         });
     });
+app.get('/get', async (req, res) => {
+    const docs = await sch.queueModel.find();
+    res.send(docs);
+})
+
+app.get('/get-one', async (req, res) => {
+    const docs = await sch.queueModel.findById(mong.Types.ObjectId(req.query.id));
+    res.send(docs);
+})
+
+var p1name;
+
+function update(name) {
+    p1name = name;
+}
+
+app.post('/addQueue', async (req, res, next) => {
+
+
+    try {
+        const q = new sch.queueModel(req.body);
+        const newQueue = await q.save();
+        //console.log(newQueue);
+        res.send(newQueue);
+
+    } catch (e) {
+        next(e);
+    };
+})
+
+app.delete('/delete', async (req, res) => {
+    const docs = await sch.queueModel.findByIdAndDelete(mong.Types.ObjectId(req.query.id));
+    res.send(docs);
+})
+
+app.put('/update', async(req, res) => {
+    console.log();
+
+    const docs = await sch.queueModel.updateOne(req.body.currentGame,req.body.newGame);
+
+    res.send(docs);
+})
+
+app.use((err, _req, res, _next) => {
+    console.error(err);
+    return res.status(500).send(err);
+});
+
+
+app.listen(8080, () => {
+    console.log('Listening on port 8080');
+});
